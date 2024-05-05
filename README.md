ed17d210c004991ab5fe0776b2e3b9cb2946c49d
knighthood  <2109695291@qq.com>
2024-05-05 14:08:03 +0800

    提交

---
+++ README.md
@@ -0,0 +1,111 @@
+# 前言
+## 项目功能
+- 通过截图软件进行截图后，通过按下`Ctrl+C`后，实现了图片提取文字，并将文字复制到剪切板中，大家通过`Ctrl+V`即可粘贴。
+## 项目优势
+- 实现了图片提取文字，并且依托微信OCR，其识别精度还是挺高的；
+- 无需登录微信，即可实现微信OCR；
+- 操作简单，项目集成了使用微信OCR进行提取文字进行粘贴的一些步骤，使得你只需要通过`Ctrl`+`C`和`V`键，即可快捷实现复制图片中的文字。
+- 可以通过更改项目中的`mode`参数，实现文字**分行复制**还是**同行复制**。
+## 项目展示
+![微信OCR](README_picture/微信OCR.mp4)
+## 初始项目架构
+下载项目打开后，项目架构如下：
+
+![img.png](README_picture/img.png)
+其中
+
+`mian.py`是项目代码。
+
+`path.txt`是配置微信OCR所需要的路径存放处。
+
+`README.md`是项目说明文档。
+
+`requirements.txt`是项目所需的包及版本。
+
+`README_picture`存放的是`README.md`项目说明文档所需要的图片。
+
+## 运行后的项目架构
+项目成功运行后，会生成`img`文件夹，里面存放的是剪切板中存放的图片，`json`文件夹中存放的是剪切板图片经过微信OCR识别后保存的json文件。
+并且会生成一个`text_save.txt`文件，里面的内容就是图片提取的文字。如下图所示：
+
+![img.png](README_picture/img_8.png)
+
+
+---
+
+# 配置环境
+要想运行本项目，首先你得先配置项目环境。
+## 创建虚拟环境
+首先打开终端，输入以下命令，创建虚拟环境
+```shell
+python -m venv venv
+```
+然后进入到这个虚拟环境，并将其激活
+```shell
+venv\scripts\activate
+```
+注意这里是反斜杠。
+
+此时可以看到，命令前面有个(venv)，说明已经进入了这个虚拟环境了。
+![img_1.png](README_picture/img_1.png)
+
+## 安装项目包
+如果你的pip版本不够，可以升级一下。命令如下
+```shell
+python -m pip install --upgrade pip
+```
+然后使用下面的命令，安装requirements.txt中的包
+```shell
+pip install -r requirements.txt
+```
+---
+## 配置python解释器
+这时候需要配置一下python解释器。当然这一步你可以选择在创建完虚拟环境后就进行这一步操作。
+![img_2.png](README_picture/img_2.png)
+点击现有环境，一般来说，它会自动帮你选择你刚刚配置好的`venv`中的`python.exe`解释器，如果没有的话，自己选择一下。
+![img_3.png](README_picture/img_3.png)
+点击确定
+![img_4.png](README_picture/img_4.png)
+可以看到，虚拟环境中已经安装了你需要的包了。
+
+以上操作完成后，你项目基础配置就完成了。
+## 配置微信OCR和mmmojo.dll路径
+找到项目中的`path.txt`文件，里面存放的是你WeChatOCR.exe和mmmojo.dll的路径
+![img_7.png](README_picture/img_7.png)
+> C:\Users\Lenovo\AppData\Roaming\Tencent\WeChat\XPlugin\Plugins\WeChatOCR\7079\extracted\WeChatOCR.exe
+![img_5.png](README_picture/img_5.png)
+> 你可以使用everything软件进行查找，并复制完整路径及文件名。
+
+> G:\applicationsoftware\WeChat\[3.9.10.19]
+![img_6.png](README_picture/img_6.png)
+> 同理，你可以复制mmmojo.dll所在的路径。
+
+**这里需要注意的是：WeChatOCR.exe的路径需要到最终文件名，mmmojo.dll的路径只需要到上一级目录即可。**
+
+然后将这两个路径，依次分行复制到`path.txt`中，保存。
+
+以上这一步操作需要好好配置，否则后续就无法运行。
+# 运行代码
+接下来就可以运行代码了
+
+# 实现同行的复制
+在main.py中的第135行中，你可以将mode改成2，这样就能将识别结果的文字放在同一行，适用与同一段落的图片的截图并提取文字。
+```python
+# TODO 你可以将mode改成2，实现文字放在同一行。
+save_text(json_file, save_file, mode=2)
+```
+# 最后
+经过以上的讲解，我觉得大家对于这个项目的配置已经运行应该是没有问题了。
+
+如有问题，欢迎＋v：TheOnewbd和我交流。
+
+**希望大家可以多多关注一下我的公众号，对这个项目的免费讲解，我会更新在微信公众号中，绝对不会让你失望。**
+
+<div style="display: flex;">
+    <div style="flex: 50%; padding: 5px;">
+        <img src="README_picture/微信.jpg" alt="微信" style="width: 100%;">
+    </div>
+    <div style="flex: 50%; padding: 5px;">
+        <img src="README_picture/公众号.jpg" alt="公众号" style="width: 100%;">
+    </div>
+</div>
---
+++ main.py
@@ -0,0 +1,152 @@
+import PIL.ImageGrab
+import os
+import json
+import time
+from wechat_ocr.ocr_manager import OcrManager, OCR_MAX_TASK_ID
+import pyperclip
+import win32api
+import win32con
+
+# 实现保存剪切板图片
+def save_clipboard_pic(save_path):
+    try:
+        img = PIL.ImageGrab.grabclipboard()
+        img.save(save_path)
+        print("剪切板中的图片已经保存")
+        return True
+    except AttributeError:
+        print("剪切板中不是图片")
+        return False
+
+def ocr_result_callback(img_path: str, results: dict):
+    result_file = os.path.join("json", os.path.basename(img_path) + ".json")  # json\01.png.json，这里是包含文件夹名称
+    print(f"识别成功，img_path: {img_path}, result_file: {result_file}")
+    with open(result_file, 'w', encoding='utf-8') as f:
+        f.write(json.dumps(results, ensure_ascii=False, indent=2))
+
+# 编写具体代码时，这个函数需要看情况封装
+def save_to_json(img_file):
+    # print(111)
+    ocr_manager = OcrManager(wechat_dir)
+    # 设置WeChatOcr目录
+    ocr_manager.SetExePath(wechat_ocr_dir)
+    # 设置微信所在路径
+    ocr_manager.SetUsrLibDir(wechat_dir)
+    # 设置ocr识别结果的回调函数
+    ocr_manager.SetOcrResultCallback(ocr_result_callback)
+    # 启动ocr服务
+    ocr_manager.StartWeChatOCR()
+    # TODO 识别图片
+    ocr_manager.DoOCRTask(img_file)
+    print("图片已经识别，保存在json文件中")
+    time.sleep(1)
+    while ocr_manager.m_task_id.qsize() != OCR_MAX_TASK_ID:
+        pass
+    # 识别输出结果
+    ocr_manager.KillWeChatOCR()
+
+def save_text(json_file, save_file, mode=1):
+    # 打开 JSON 文件
+    with open(json_file, 'r', encoding='utf-8') as file:
+        # 从文件中加载 JSON 数据
+        data = json.load(file)
+    print("--------------识别内容----------------")
+    # 换行保存
+    if mode == 1:
+        with open(save_file, 'w', encoding='utf-8') as f:
+            # 提取每个对象的 text 字段
+            for item in data['ocrResult']:
+                print(item['text'])
+                f.write(item['text'] + '\n')
+    # 不换行保存
+    if mode == 2:
+        with open(save_file, 'w', encoding='utf-8') as f:
+            # 提取每个对象的 text 字段
+            for item in data['ocrResult']:
+                print(item['text'])
+                f.write(item['text'])
+    print("------------------------------------")
+
+def txt_copy(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        # 读取文件内容
+        file_content = file.read()
+    # 将文件内容复制到剪贴板
+    pyperclip.copy(file_content)
+    print("已将文件内容复制到剪贴板。")
+
+## 读取path.txt中你放的文件路径
+def read_path(file, line=1):
+    with open(file, 'r') as file:
+        lines = file.readlines()
+        select_line = lines[line-1]
+        # 添加 r 前缀
+        select_line = r"{}".format(select_line.strip())
+        # print(select_line)
+        return select_line
+
+if __name__ == '__main__':
+    # TODO:创建对应文件夹
+    if not os.path.exists("img"):   # 自动创建img文件夹，其中存放剪切板的图片
+        os.makedirs("img")
+    if not os.path.exists("json"):  # 自动创建json文件夹，其中存放剪切板的图片文字识别后的json文件
+        os.makedirs("json")
+    path = "path.txt"
+    # TODO：读取文件路径
+    wechat_ocr_dir = read_path(path, line=1)
+    wechat_dir = read_path(path, line=2)
+    # TODO:配置并启动OCR服务
+    ocr_manager = OcrManager(wechat_dir)
+    # 设置WeChatOcr目录
+    ocr_manager.SetExePath(wechat_ocr_dir)
+    # 设置微信所在路径
+    ocr_manager.SetUsrLibDir(wechat_dir)
+    # 设置ocr识别结果的回调函数
+    ocr_manager.SetOcrResultCallback(ocr_result_callback)
+    # 启动ocr服务
+    ocr_manager.StartWeChatOCR()
+
+
+    # 监视Ctrl+C 和 Esc 键
+    ctrl_c_pressed = False
+    esc_pressed = False
+
+    try:
+        print("按下 Ctrl+C 实现微信OCR识别图片并复制到剪切板")
+        print("按下 Esc 退出程序")
+        while not esc_pressed:
+            # 获取键盘状态，检查 Ctrl+C 和 Esc 键是否被按下
+            if win32api.GetAsyncKeyState(ord('C')) and win32api.GetAsyncKeyState(win32con.VK_CONTROL):
+                ctrl_c_pressed = True
+                print("Ctrl+C 被按下")
+                img_file = r"img\~~~ocr.png"
+                result = save_clipboard_pic(img_file)
+                if result:  # True表示剪切板图片已经保存，False表示剪切板根本不是图片
+                    file_name = os.path.basename(img_file)  # 使用 os.path.basename() 函数获取文件名
+                    json_file = os.path.join("json", file_name + ".json")  # 通过上述操作将01.png变成json\01.png.json
+                    save_file = "text_save.txt"
+                    # save_to_json(img_file)
+                    # TODO 识别图片
+                    ocr_manager.DoOCRTask(img_file)
+                    print("图片已经识别，保存在json文件中")
+                    time.sleep(1)
+                    while ocr_manager.m_task_id.qsize() != OCR_MAX_TASK_ID:
+                        pass
+                    # TODO 你可以将mode改成2，实现文字放在同一行。
+                    save_text(json_file, save_file, mode=1)
+                    txt_copy(save_file)  # 文本复制到剪切板
+                # else:
+                #     print("剪切板中不包含有效图片")
+                #     pass
+
+            # 检查 Esc 键是否被按下
+            if win32api.GetAsyncKeyState(win32con.VK_ESCAPE):
+                esc_pressed = True
+                # 关闭微信OCR
+                ocr_manager.KillWeChatOCR()
+                print("Esc 键被按下，准备退出...")
+            time.sleep(0.1)  # 在此处可以执行其他任务
+    except KeyboardInterrupt:
+        print("手动中断")
+    print("程序结束")
+
